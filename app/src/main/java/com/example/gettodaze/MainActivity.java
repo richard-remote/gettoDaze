03f6b2f4a1870508bb19e4179a91b57e4eab24da
richard-remote <clarkeril8082@gmail.com>
2019-07-31 12:21:49 -0400

    Setup popup xml Added TextView and EditText to test how the Popup will render

--- app/src/main/java/com/example/gettodaze/MainActivity.java
+++ app/src/main/java/com/example/gettodaze/MainActivity.java
@@ -2,24 +2,17 @@ package com.example.gettodaze;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
-import android.view.View;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
-    TextView tv;
-    String text;
+public class MainActivity extends AppCompatActivity{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    public void touch(View v){
-        text = ((TextView) v).getText().toString();
-
-            if (text.equals("empty"))
-                text = "Leaving this alone for now";
+    public void press(){
+        String text;
     }
-    
 }
