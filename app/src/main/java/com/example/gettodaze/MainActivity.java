a4f8b47a3b96b15300c49500820f75fbfbc47ae9
richard-remote <clarkeril8082@gmail.com>
2019-07-31 12:51:11 -0400

    OnClick experiment Tried to setContentView when the EditText object contains a distinct string onClick This only works when I click enter after typing, AND if I click the EditText field

--- app/src/main/java/com/example/gettodaze/MainActivity.java
+++ app/src/main/java/com/example/gettodaze/MainActivity.java
@@ -1,8 +1,12 @@
+//7-31 Tried to setContentView when the EditText object contains a distinct string onClick
+//      This only works when I click enter after typing, AND if I click the EditText field
+
 package com.example.gettodaze;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.EditText;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity{
 
@@ -12,7 +16,11 @@ public class MainActivity extends AppCompatActivity{
         setContentView(R.layout.activity_main);
     }
 
-    public void press(){
-        String text;
+    public void press(View v){
+        String text = ((TextView) v).getText().toString();
+            if (text.equals("black"))
+                setContentView(R.layout.black_screen);
+
+
     }
 }
