9da647b2cec4b97b3f8c1c46a32734bfb1806f6c
richard-remote <clarkeril8082@gmail.com>
2019-07-31 17:57:08 -0400

    Copied the possible solution Unclear if it will work. Messing around still

--- app/src/main/java/com/example/gettodaze/MainActivity.java
+++ app/src/main/java/com/example/gettodaze/MainActivity.java
@@ -3,24 +3,62 @@
 
 package com.example.gettodaze;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.View.OnKeyListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends Activity implements OnKeyListener{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText input = (EditText) findViewById(R.id.input);
+        input.setOnKeyListener(this);
     }
 
-    public void press(View v){
-        String text = ((TextView) v).getText().toString();
-            if (text.equals("black"))
-                setContentView(R.layout.black_screen);
+    @Override
+    public boolean onKey(View view, int keyCode, KeyEvent event) {
 
+        TextView responseText = (TextView) findViewById(R.id.responseText);
+        EditText myEditText = (EditText) view;
+
+        if (keyCode == EditorInfo.IME_ACTION_SEARCH ||
+                keyCode == EditorInfo.IME_ACTION_DONE ||
+                event.getAction() == KeyEvent.ACTION_DOWN &&
+                        event.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
+
+            if (!event.isShiftPressed()) {
+                Log.v("AndroidEnterKeyActivity","Enter Key Pressed!");
+                //if
+                /*
+                switch (view.getId()) {
+                    case R.id.editText:
+                        responseText
+                                .setText("Just pressed the ENTER key, " +
+                                        "focus was on Text Box1. " +
+                                        "You typed:\n" + myEditText.getText());
+                        break;
+                    case R.id.editText2:
+                        responseText
+                                .setText("Just pressed the ENTER key, " +
+                                        "focus was on Text Box2. " +
+                                        "You typed:\n" + myEditText.getText());
+                        break;
+                }
+                */
+                return true;
+            }
+
+        }
+        return false; // pass on to other listeners.
 
     }
 }
